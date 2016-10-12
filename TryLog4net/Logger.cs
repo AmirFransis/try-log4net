b4ba95c8d95fc1cf6a87adac505ec60e9e98fba4
Rodrigo Campos <rodrigo.campos@intelitrader.com.br>
2016-10-12 15:18:54 -0300

    Adiciona fontes ao controle de versao

---
+++ TryLog4net/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TryLog4net
+{
+    public static class Logger
+    {
+        private static readonly log4net.ILog _performance =
+            log4net.LogManager.GetLogger("PERFORMANCE");
+
+        private static readonly log4net.ILog _info =
+            log4net.LogManager.GetLogger("INFO");
+
+        private static readonly log4net.ILog _debug =
+            log4net.LogManager.GetLogger("DEBUG");
+
+        private static readonly log4net.ILog _error =
+            log4net.LogManager.GetLogger("ERROR");
+
+        static Logger()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public static void Info(string msg) => _info.Info(msg);
+        public static void Debug(string msg) => _debug.Debug(msg);
+        public static void Exception(Exception ex) => _error.Fatal(ex.ToString());
+        public static void Performance(string msg) => _performance.Info(msg);
+    }
+}
---
+++ TryLog4net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TryLog4net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Debug("debug");
+            Logger.Info("info");
+            Logger.Performance("performance");
+            Logger.Exception(new Exception("exception"));
+        }
+    }
+}
